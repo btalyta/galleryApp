04f675d80a108902627e2a55bc85c90372471d82
Bárbara Souza <b.barbaratallyta@gmail.com>
2021-02-14 15:06:57 -0300

    Add tests to GetSizesEndpoint

--- GalleryApp/GalleryAppTests/CoreService/GetSizes/GetSizesEndpointTests.swift
+++ GalleryApp/GalleryAppTests/CoreService/GetSizes/GetSizesEndpointTests.swift
@@ -10,10 +10,10 @@ import XCTest
 
 @testable import GalleryApp
 
-class ConnectionsEndpointTests: XCTestCase {
+class GetSizesEndpointTests: XCTestCase {
     func test_url_returnsCorrectValue() {
         let sut = createSut()
-        XCTAssertEqual(sut.url, "https://farm6.staticflickr.com/5800/31456463045_5a0af4ddc8_q.jpg")
+        XCTAssertEqual(sut.url, "https://api.flickr.com/services/rest/")
     }
 
     func test_method_returnsGET() {
@@ -34,14 +34,20 @@ class ConnectionsEndpointTests: XCTestCase {
         XCTAssertEqual(sut.headers, expectedHeader)
     }
 
-    func test_queries_returnsEmpty() {
+    func test_queries_returnsCorrectValues() {
         let sut = createSut()
-        let expectedQueries: [String: String] = [:]
+        let expectedQueries = [
+            "api_key": "123456789",
+            "nojsoncallback": "1",
+            "format": "json",
+            "photo_id": "ABC123",
+            "method": "flickr.photos.getSizes"
+        ]
 
         XCTAssertEqual(sut.queries, expectedQueries)
     }
 
-    func createSut() -> ImageEndpoint {
-        return ImageEndpoint(url: "https://farm6.staticflickr.com/5800/31456463045_5a0af4ddc8_q.jpg")
+    func createSut() -> GetSizesEndpoint {
+        return GetSizesEndpoint(apiKey: "123456789", photoId: "ABC123")
     }
 }
