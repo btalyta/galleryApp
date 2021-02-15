812f3c8356514651245a30a9673527254392c444
Bárbara Souza <b.barbaratallyta@gmail.com>
2021-02-15 14:53:21 -0300

    Add PhotosViewModel

---
+++ GalleryApp/GalleryApp/Scene/GalleryView/View/PhotosViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoViewModel.swift
+//  GalleryApp
+//
+//  Created by Barbara Souza on 14/02/21.
+//
+
+import Foundation
+
+struct PhotosViewModel {
+    var photos: [Photo] = []
+    var hasMore: Bool = true
+    var isLoading: Bool = false
+}
