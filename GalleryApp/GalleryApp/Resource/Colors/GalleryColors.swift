34ba10bab6a65007d671cefb83f06827220fcfc3
Bárbara Souza <b.barbaratallyta@gmail.com>
2021-02-15 12:08:53 -0300

    Add app colors

---
+++ GalleryApp/GalleryApp/Resource/Colors/GalleryColors.swift
@@ -0,0 +1,12 @@
+//
+//  GalleryColors.swift
+//  GalleryApp
+//
+//  Created by Barbara Souza on 15/02/21.
+//
+
+import UIKit
+
+class GalleryColors {
+    static let loadColor = UIColor.lightGray.withAlphaComponent(0.3)
+}
