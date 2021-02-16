fdbd19c8ccc12e19f9ff9ed4959a34d4391bf594
Bárbara Souza <b.barbaratallyta@gmail.com>
2021-02-16 08:19:32 -0300

    Add PhotoDetailView

---
+++ GalleryApp/GalleryApp/Scene/PhotoDetail/View/PhotoDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoDetailViewModel.swift
+//  GalleryApp
+//
+//  Created by Barbara Souza on 16/02/21.
+//
+
+import UIKit
+
+struct PhotoDetailViewModel: Equatable {
+    let image: UIImage
+    let title: String
+}
